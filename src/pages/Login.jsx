84b5dd5050ef1e1d59d2514270b395333ceeef23
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-20 21:30:19 +0530

    reqres auth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,19 @@
 // import { AuthContext } from "../context/AuthContext";
 import React from "react";
 const Login = () => {
+  let [users, setUsers] = React.useState([]);
+  let url = "https://reqres.in/api/login";
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    setUsers(data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  const verifyUser =()=>{
+    console.log(users);
+  }
   return <div>
     <h1>Login</h1>
     <label>Email :</label>
@@ -11,8 +24,9 @@ const Login = () => {
     <br />
     <label>Password</label>
     <input placeholder="#1@Ab0">
-
     </input>
+    <br />
+    <button onClick={verifyUser}>LOGIN</button>
   </div>;
 };
 
