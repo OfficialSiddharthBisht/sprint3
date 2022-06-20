8019197e5b260e35c9eeac10e79c68b8b4045fde
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-20 20:57:30 +0530

    started product item using fetch

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ProductItem = () => {
-  return <div>{/* CODE HERE */}</div>;
+  let url = "http://localhost:8080/products";
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  return <div>
+    
+  </div>;
 };
 
 export default ProductItem;
