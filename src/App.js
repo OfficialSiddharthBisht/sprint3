7575e5b1a06a9f5b0eef7f3afdb56bb4ab4cef73
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-20 20:19:16 +0530

    initaila commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="App">{/* CODE HERE */}</div>;
+  return <div className="App">
+    <Navbar />
+  </div>;
 }
 
 export default App;
