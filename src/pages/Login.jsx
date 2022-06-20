4dfd4b3eedd2904a5dc5f72ce0e84935ca292316
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-20 20:44:27 +0530

    setup input box for email and password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,15 @@ import React, { useState, useContext } from "react";
 const Login = () => {
   return <div>
     <h1>Login</h1>
+    <label>Email :</label>
+    <input placeholder="elonmusk@abc.com">
+
+    </input>
+    <br />
+    <label>Password</label>
+    <input placeholder="#1@Ab0">
+
+    </input>
   </div>;
 };
 
