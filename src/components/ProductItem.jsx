a9779453200fdb90852d1cee1a726808f1a9c39c
Siddharth <officialsiddharthbisht@gmail.com>
2022-06-20 21:08:11 +0530

    displayed all the product from json server

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,16 +2,24 @@ import React from "react";
 
 const ProductItem = () => {
   let url = "http://localhost:8080/products";
+  let [prod, setProd] = React.useState([]);
   fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    let products = data.map(product => {
+      return <div>
+        <h2>{product.name}</h2>
+        <p>{product.description}</p>
+        <button>ADD TO CART</button>
+      </div>
+    });
+    setProd(products);
   })
   .catch(error => {
     console.log(error);
   })
   return <div>
-    
+    {prod}
   </div>;
 };
 
